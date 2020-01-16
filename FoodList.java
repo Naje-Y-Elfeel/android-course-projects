f8045c539e55384988237d618bdf7fdc7fa672b4
Naje Yagoub Elfeel Ogla <47554251+Naje-Y-Elfeel@users.noreply.github.com>
2020-01-16 14:47:38 +0200

    Create FoodList.java

---
+++ FoodList.java
@@ -0,0 +1,28 @@
+package com.example.naje.sudanesefood;
+
+public final class FoodList {
+    public static String[] getFoodName() {
+        return new String[] {
+                "Qurrasa",
+                "Aseeda",
+                "Kissra",
+                "Fool",
+                "Tameeya",
+                "Baleela",
+                "Agashai",
+                "Mulah Bamya",
+                "Mulah Rejla",
+                "Mulah Fasulia",
+                "Mulah Batates",
+                "Mulah Neaimeya",
+                "Mulah Rob",
+                "Mulah Tagaleya",
+                "Shayya",
+                "Kawaree",
+                "Kibda",
+                "Shorbat Daan",
+                "Mukhbaza",
+                "Bakumba"
+        };
+    }
+}
